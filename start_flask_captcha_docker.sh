6ec15b61c3edbea166f97d2f142ffde5a8fa19f4
Trunov Pavel <trunovpavel@yandex.ru>
2021-07-22 21:28:54 +0300

    update start_flask_captcha_docker.sh

--- start_flask_captcha_docker.sh
+++ start_flask_captcha_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v /home/pavel/PycharmProjects/Simple_capcha_solver_with_flask_docker/flask_service:/flask_service -p 5000:5000 flask_captcha /bin/bash -c 'cd ../flask_service && ./start_gunicorn.sh'
+docker run --rm -it -v $PWD/flask_service:/flask_service -p 5000:5000 flask_captcha /bin/bash -c 'cd ../flask_service && ./start_gunicorn.sh'
